2dce8380a9b9718706fbd154065767de73bd0e61
yuliang <891582612@qq.com>
2021-02-08 09:47:29 +0800

    init

--- src/main/java/com/excel/demo/getExcel/controller/ExcelController.java
+++ src/main/java/com/excel/demo/getExcel/controller/ExcelController.java
@@ -1,6 +1,8 @@
 package com.excel.demo.getExcel.controller;
 
 
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.ExportParams;
 import com.excel.demo.getExcel.entity.SysAccount;
 import com.excel.demo.getExcel.entity.SysOrg;
 import com.excel.demo.getExcel.entity.SysUser;
@@ -8,16 +10,22 @@ import com.excel.demo.getExcel.service.ISysAccountService;
 import com.excel.demo.getExcel.service.ISysOrgService;
 import com.excel.demo.getExcel.service.ISysUserService;
 import com.excel.demo.getExcel.utils.PoiUtils;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.Action;
 import java.io.*;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/excel")
@@ -58,4 +66,18 @@ public class ExcelController {
         List<SysOrg> sysOrgs = PoiUtils.importOrg2List(multipartFile);
         sysOrgService.saveBatch(sysOrgs);
     }
+
+    /*@PostMapping("/excel")
+    public void excel(HttpServletResponse response, HttpServletRequest request,
+                      @RequestBody Map<String, String> params) throws Exception {
+        QueryWrapper<SerBossPubFreeNum> query = getQuery(params);
+        List<SerBossPubFreeNum> data = service.list(query);
+        Workbook workbook = ExcelExportUtil.exportExcel(new ExportParams(null, "SerBossPubFreeNum"),
+                SerBossPubFreeNum.class, data);
+        String fileName = String.format("SerBossPubFreeNum_%d.xls", System.currentTimeMillis());
+        response.setHeader("Content-Disposition", "attachment;Filename="+ fileName);
+        response.setContentType("application/vnd.ms-excel;charset=UTF-8");
+        response.flushBuffer();
+        workbook.write(response.getOutputStream());
+    }*/
 }
